0bb88a023ef17f489e68f735f6acfe5d204b33fe
Myung Shin Kim <himskim@msn.com>
2016-04-15 00:21:09 +0800

    create client app

---
+++ EmotionLib/HappyModel/HappyModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+namespace Happy
+{
+    public class HappyModel : Scores
+    {
+        public HappyModel(string storeID, Emotion emotion)
+        {
+            this.storeID = storeID;
+            this.time = DateTime.UtcNow;
+
+            this.Anger = emotion.Scores.Anger;
+            this.Contempt = emotion.Scores.Contempt;
+            this.Disgust = emotion.Scores.Disgust;
+            this.Fear = emotion.Scores.Fear;
+            this.Happiness = emotion.Scores.Happiness;
+            this.Neutral = emotion.Scores.Neutral;
+            this.Sadness = emotion.Scores.Sadness;
+            this.Surprise = emotion.Scores.Surprise;
+        }
+
+        public string storeID { get; private set; }
+        public DateTime time { get; private set; }
+    }
+}
---
+++ EmotionLib/HappyTestClient/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HappyTestClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            Happy.EmotionClient client = new Happy.EmotionClient();
+            Happy.HappyModel model = await client.RecognizeAsync("store1", "http://hydroday.com/wp-content/uploads/2014/10/This-man-thinks-his-spotted.jpg");
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("storeID : {0}\n", model.storeID);
+            sb.AppendFormat("time : {0}\n", model.time);
+            sb.AppendFormat("Anger : {0}\n", model.Anger);
+            sb.AppendFormat("Contempt : {0}\n", model.Contempt);
+            sb.AppendFormat("Disgust : {0}\n", model.Disgust);
+            sb.AppendFormat("Fear : {0}\n", model.Fear);
+            sb.AppendFormat("Happiness : {0}\n", model.Happiness);
+            sb.AppendFormat("Neutral : {0}\n", model.Neutral);
+            sb.AppendFormat("Sadness : {0}\n", model.Sadness);
+            sb.AppendFormat("Surprise : {0}\n", model.Surprise);
+
+            textbox.Text = sb.ToString();
+        }
+    }
+}
